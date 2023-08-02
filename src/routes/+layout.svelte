a49650238e2202e2ec58ad0953f3c17d06d29247
Up <upcraftedlp@gmail.com>
2023-08-03 00:52:38 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "../app.postcss";
+  const year = new Date().getFullYear();
+  const copyrightStartYear = 2016;
+	const copyrightYear = year === copyrightStartYear ? year : `${copyrightStartYear}-${year}`;
+  const copyrightName = "Mod Dev Zone";
+</script>
+
+<main class="my-auto flex flex-grow">
+  <slot />
+</main>
+
+<footer class="flex mt-auto bg-slate-600 text-slate-300">
+  <p class="self-center p-1 m-auto text-sm">
+    Copyright &COPY; {copyrightYear} {copyrightName}, All rights reserved.
+  </p>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex-grow flex bg-gradient-to-tr from-fuchsia-600 via-pink-500 via-45% to-purple-400 to-95%">
+    <div class="self-center m-auto text-center text-fuchsia-950">
+        <h1 class="text-3xl underline font-extrabold">Mod Dev Zone</h1>
+        <div class="self-center m-auto text-3xl italic  bg-fuchsia-200  w-fit p-5 rounded-xl">
+            <h1>Under Construction</h1>
+            Come back later.
+        </div>
+    </div>
+</div>
